1a89d28aed406593c99f8bdc6361197557e048db
escakot <ec@neorennie.com>
2017-06-30 19:22:54 -0400

    Initial Commit
    Added InputHandler class
    Create game loop and quit function

--- Snakes and Ladders/main.m
+++ Snakes and Ladders/main.m
@@ -7,11 +7,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "InputHandler.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    
+        NSLog(@"Welcome to Snakes & Ladders\n");
+        NSLog(@"Game Commands:\n"
+              "roll - Roll dice\n");
+        BOOL playGame = YES;
+        do {
+            NSString* inputString = [InputHandler obtainInput];
+            
+            if ([inputString isEqualToString:"quit"]){
+                NSLog(@"Game has ended");
+                playGame = NO;
+            }
+            
+        }while(playGame);
+    
     }
     return 0;
 }
